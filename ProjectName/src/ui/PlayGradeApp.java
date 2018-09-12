93809c0bd107213250d5b6e3ac6375f64158eaf5
Mary Cheung <t9x0b@ugrad.cs.ubc.ca>
2018-09-11 21:44:07 -0700

    2. Basic interaction. I made a student class and called a constructor for Student.

--- ProjectName/src/ui/PlayGradeApp.java
+++ ProjectName/src/ui/PlayGradeApp.java
@@ -4,7 +4,13 @@ public class PlayGradeApp {
 
     public static void main(String[] args) {
         Student student = new Student("Mary",10,20);
-        System.out.println(student.name + "'s mark is " + student.calculateGrade() + "%");
+        Student student0 = new Student("Angela", 20,20);
+        printMark(student);
+        printMark(student0);
+    }
+
+    private static void printMark(Student student) {
+        System.out.println(student.name + "'s mark is " + student.calculateGrade() + "%.");
     }
 
 
